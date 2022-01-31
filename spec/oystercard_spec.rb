a06ebc1eba7162af4637c9cbec103ae3bb62a545
Ben Partington <bdpartington@gmail.com>
2022-01-31 17:35:38 +0000

    Created oystercard class. Required oystercard.rb in oystercard_spec

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,2 +1,14 @@
+require_relative '../lib/oystercard.rb'
+
 describe Oystercard do
+  #freshly initialized
+  #has balance of 0 by default
+
 end
+
+
+
+# Notes on Stack Trace:
+# NameError - unitialized constant Oystercard
+# ./spec/oystercard_spec
+# rb:1:in
