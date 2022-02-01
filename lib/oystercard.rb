c93dbe506e1f95d150a67607c6fbbd44f0649292
Jaroslaw Roman Cudzich <internet.jarek@gmail.com>
2022-02-01 14:51:14 +0000

    Created method and rspec tests for topping up the oyster card.

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,5 +1,15 @@
 class Oystercard
-    def initialize
+
+  attr_reader :balance
+
+  def initialize
     @balance = 0
-    end
+  end
+
+  def top_up(amount)
+    @balance += amount
+  end
+
 end
+
+
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,13 +1,33 @@
 require_relative '../lib/oystercard.rb'
 
 describe Oystercard do
-  describe "#top_up" do  
-    it "should add money to the oyster card" do
-      oystercard = Oystercard.new
-      expect(oystercard).to respond_to(:top_up).with(1).argument
+  describe "#balance"
+  it "has a balance of zero" do
+    expect(subject.balance).to eq(0)
+  end
+  
+  describe "#top_up" do
+  it "balance of card should increase by given value" do
+    # subject.top_up(10)
+    # expect(subject.balance).to eq(10)
+    expect{ subject.top_up 10 }.to change{ subject.balance }.by 10 
+    # expect { a += 1 }.to change { a }.by(1)
     end
   end
 end
+
+  
+
+
+
+
+#   describe "#top_up" do  
+#     it "should add money to the oyster card" do
+#       oystercard = Oystercard.new
+#       expect(oystercard).to respond_to(:top_up).with(1).argument
+#     end
+#   end
+# end
 
 
 
