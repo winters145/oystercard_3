02be8ad5ee6956572a756269407b55c191c88922
Jack <winterschladen.j@gmail.com>
2022-02-01 15:27:39 +0000

    Created a MAXIMUM_BALANCE error to raise when oystercard top_up exceeds it

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,15 +1,14 @@
 class Oystercard
 
   attr_reader :balance
-
+  MAXIMUM_BALANCE = 90
   def initialize
     @balance = 0
   end
 
   def top_up(amount)
+    raise "Unable to top up: maximum balance is £#{MAXIMUM_BALANCE}" if @balance + amount > MAXIMUM_BALANCE
     @balance += amount
   end
 
 end
-
-
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -2,34 +2,20 @@ require_relative '../lib/oystercard.rb'
 
 describe Oystercard do
   describe "#balance"
-  it "has a balance of zero" do
-    expect(subject.balance).to eq(0)
-  end
+    it "has a balance of zero" do
+      expect(subject.balance).to eq(0)
+    end
   
   describe "#top_up" do
-  it "balance of card should increase by given value" do
-    # subject.top_up(10)
-    # expect(subject.balance).to eq(10)
-    expect{ subject.top_up 10 }.to change{ subject.balance }.by 10 
-    # expect { a += 1 }.to change { a }.by(1)
+    it "balance of card should increase by given value" do
+      expect{ subject.top_up 10 }.to change{ subject.balance }.by 10 
+      end
+
+    it "should raise an error if the top_up amount takes the balance greater than maximum" do
+      expect { subject.top_up(Oystercard::MAXIMUM_BALANCE+1) }.to raise_error "Unable to top up: maximum balance is £#{Oystercard::MAXIMUM_BALANCE}"
     end
   end
 end
-
-  
-
-
-
-
-#   describe "#top_up" do  
-#     it "should add money to the oyster card" do
-#       oystercard = Oystercard.new
-#       expect(oystercard).to respond_to(:top_up).with(1).argument
-#     end
-#   end
-# end
-
-
 
 # Notes on Stack Trace:
 # NameError - unitialized constant Oystercard
